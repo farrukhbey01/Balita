0ca2210e561e4f33ad5027144ac632639678adda
Farruh64 <rixsiboyevfaruuh@gmail.com>
2024-07-09 18:52:20 +0500

    change

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.views.static import serve
 from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
-
+from blog.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
